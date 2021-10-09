14b35c2b6c6c5020d990d47fc8b71e26253ecbc4
Cobas91 <stephan.gaertner72@outlook.de>
2021-10-09 13:31:37 +0200

    Delete not used imports

--- backend/src/test/java/de/neuefische/backend/controller/QuestionControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/QuestionControllerTest.java
@@ -10,12 +10,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+
 
 import java.util.List;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class QuestionControllerTest {
